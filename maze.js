89dfd3dfee150940f8dea27a49e14ba9fcd15551
MoniqueCampbell <campbellmonique48@yahoo.com>
2018-10-12 13:06:09 -0500

    Single boundary
    Single boundary turns red when the user moves the mouse onto a single wall of the maze wall.

---
+++ maze.js
@@ -0,0 +1,12 @@
+
+
+
+	window.onload = function(){
+		// document.addEventListener("mouseover",document.getElementsByClassName("div.youlose")
+		
+		document.getElementById("boundary1").onmouseover = function(){
+
+			document.getElementById("boundary1").classList.add("youlose");
+		}
+	}
+
