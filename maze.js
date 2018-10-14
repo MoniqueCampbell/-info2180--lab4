d731ef939d397f652f758ee58604a495636d8856
MoniqueCampbell <campbellmonique48@yahoo.com>
2018-10-14 12:05:59 -0500

    Question 2
    Got all the divs to glow red when mouse hovers over it.

--- maze.js
+++ maze.js
@@ -3,10 +3,36 @@
 
 	window.onload = function(){
 		// document.addEventListener("mouseover",document.getElementsByClassName("div.youlose")
-		
-		document.getElementById("boundary1").onmouseover = function(){
+
+		/*document.getElementById("boundary1").onmouseover = function(){
 
 			document.getElementById("boundary1").classList.add("youlose");
-		}
-	}
+			console.log(5+8);*/
+			
+			/*document.querySelectorAll(".boundary").onmouseover= function(){
+window.alert(7);
+				document.getElementById("boundary").classList.add("youlose")*/
+			document.querySelector(".boundary").onmouseover = function(){
+					var x=document.querySelectorAll(".boundary");
+				var i;
+				for (i=0;i<x.length;i++){
+					x[i].classList.add("youlose"); 
+					}
+					
+				}
+				/*document.getElementById("end").onmouseover = function(){
+
+			window.alert("You lose");
+			<h2 id="status">"You win"</h2>
+			<h2 id="status">"You lose"</h2>
+			document.getElementById("status").innerHTML="You win";
+			document.getElementById("status").innerHTML="You lose";*/
+				};
+			
+
+		
+
+
+		
+
 
