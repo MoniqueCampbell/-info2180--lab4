8911e28201467b54075493b2eb56008883ece76b
MoniqueCampbell <campbellmonique48@yahoo.com>
2018-10-22 09:03:48 -0500

    Part 4
    The maze is restartable.

--- maze.js
+++ maze.js
@@ -3,13 +3,13 @@
 
 	window.onload = function(){
 		
-
+		//Part 1
 		/*document.getElementById("boundary1").onmouseover = function(){
 
 			document.getElementById("boundary1").classList.add("youlose");*/
 			
 			
-			
+			//Part2
 			document.querySelector(".boundary").onmouseover = function(){
 					var x=document.querySelectorAll(".boundary");
 				var i;
@@ -19,6 +19,7 @@
 					
 				}
 				/*Line 24 and 26 works*/
+				//Part3
 				document.getElementById("end").onmouseover = function(){
 				if (document.getElementsByClassName("youlose").length === 0){
 					window.alert("You win");
@@ -28,27 +29,22 @@
 			}
 
 
-
-			/*document.getElementById("start").onclick = function(){
+			//Part4
+			document.getElementById("start").onclick = function(){
 			var m = document.getElementsByClassName("boundary").length - 1;
-			var p;
-			for (p=0;p>m.length;m--){
-					m[p].classList.remove("youlose"); 
-					}
-					
-				}*/
-		/*while (m >= 0)
-		{
-			document.querySelectorAll(".boundary")[i].classList.remove("youlose");
-	 		m--;
-	    }*/
+			while (m >= 0)
+			{
+				document.querySelectorAll(".boundary")[m].classList.remove("youlose");
+	 			m--;
+	    	}
+		
 			/*<h2 id="status">"You win"</h2>
 			<h2 id="status">"You lose"</h2>
 			document.getElementById("status").innerHTML="You win";
 			document.getElementById("status").innerHTML="You lose";
 				} };
 			*/
-		}
+		}}
 
 		
 
