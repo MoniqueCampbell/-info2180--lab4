e29475c109a57ea10320fe9177cd2b651ce7578e
MoniqueCampbell <campbellmonique48@yahoo.com>
2018-10-22 08:06:47 -0500

    Part 3
    I got the end button to display you win only if the user never hit any of the walls.

--- maze.js
+++ maze.js
@@ -2,16 +2,14 @@
 
 
 	window.onload = function(){
-		// document.addEventListener("mouseover",document.getElementsByClassName("div.youlose")
+		
 
 		/*document.getElementById("boundary1").onmouseover = function(){
 
-			document.getElementById("boundary1").classList.add("youlose");
-			console.log(5+8);*/
+			document.getElementById("boundary1").classList.add("youlose");*/
 			
-			/*document.querySelectorAll(".boundary").onmouseover= function(){
-window.alert(7);
-				document.getElementById("boundary").classList.add("youlose")*/
+			
+			
 			document.querySelector(".boundary").onmouseover = function(){
 					var x=document.querySelectorAll(".boundary");
 				var i;
@@ -20,15 +18,19 @@ window.alert(7);
 					}
 					
 				}
-				/*document.getElementById("end").onmouseover = function(){
-
-			window.alert("You lose");
-			<h2 id="status">"You win"</h2>
+				/*Line 24 and 26 works*/
+				document.getElementById("end").onmouseover = function(){
+if (document.getElementsByClassName("youlose").length === 0)
+			{
+			window.alert("You win");
+			}
+			/*<h2 id="status">"You win"</h2>
 			<h2 id="status">"You lose"</h2>
 			document.getElementById("status").innerHTML="You win";
-			document.getElementById("status").innerHTML="You lose";*/
-				};
-			
+			document.getElementById("status").innerHTML="You lose";
+				} };
+			*/
+		}}
 
 		
 
