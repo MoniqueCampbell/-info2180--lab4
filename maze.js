9b28a1ef381f4fca80bd0907d9976cce95330852
MoniqueCampbell <campbellmonique48@yahoo.com>
2018-10-22 18:01:57 -0500

    Part 5
    I got the once alert messages to appear in the browser as h2.

--- maze.js
+++ maze.js
@@ -1,50 +1,41 @@
-
-
-
-	window.onload = function(){
+window.onload = function(){
 		
-		//Part 1
-		/*document.getElementById("boundary1").onmouseover = function(){
-
-			document.getElementById("boundary1").classList.add("youlose");*/
+	//Part 1
+	document.getElementById("boundary1").onmouseover = function(){
+		document.getElementById("boundary1").classList.add("youlose");
+	}	
 			
-			
-			//Part2
-			document.querySelector(".boundary").onmouseover = function(){
-					var x=document.querySelectorAll(".boundary");
-				var i;
-				for (i=0;i<x.length;i++){
-					x[i].classList.add("youlose"); 
-					}
-					
-				}
-				/*Line 24 and 26 works*/
-				//Part3
-				document.getElementById("end").onmouseover = function(){
-				if (document.getElementsByClassName("youlose").length === 0){
-					window.alert("You win");
-			} else{
-				window.alert("You lose");
-				}
-			}
+	//Part 2 and Part 5
+	document.querySelector(".boundary").onmouseover = function(){
+		var x=document.querySelectorAll(".boundary");
+		var i;
+		for (i=0;i<x.length;i++){
+			x[i].classList.add("youlose"); 
+		}	
+		document.body.append(document.createElement("h2").textContent = "You Lose!");			
+	}
+				
+	//Part 3 and Part 5
+	document.getElementById("end").onmouseover = function(){
+		if (document.getElementsByClassName("youlose").length === 0){
+			/*window.alert("You win");*/
+			 document.body.append(document.createElement("h2").textContent = "You Win!");
+		}
+		else{
+			/*window.alert("You lose");*/
+			document.body.append(document.createElement("h2").textContent = "You Lose!");
+		}
+	}
 
-
-			//Part4
-			document.getElementById("start").onclick = function(){
-			var m = document.getElementsByClassName("boundary").length - 1;
-			while (m >= 0)
-			{
-				document.querySelectorAll(".boundary")[m].classList.remove("youlose");
-	 			m--;
-	    	}
-		
-			/*<h2 id="status">"You win"</h2>
-			<h2 id="status">"You lose"</h2>
-			document.getElementById("status").innerHTML="You win";
-			document.getElementById("status").innerHTML="You lose";
-				} };
-			*/
-		}}
+	//Part 4
+	document.getElementById("start").onclick = function(){
+		var m = document.getElementsByClassName("boundary").length - 1;
+		while (m >= 0){
+			document.querySelectorAll(".boundary")[m].classList.remove("youlose");
+	 		m--;
+	    }
+	}
+}
 
 		
 
