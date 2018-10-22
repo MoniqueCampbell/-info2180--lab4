516a46772641522fa67337de1774eaf0e0573c54
MoniqueCampbell <campbellmonique48@yahoo.com>
2018-10-22 08:36:17 -0500

    an update to part 3
    I did not realise my maze did not alert the user with a you lose message if they touched the maze wall

--- maze.js
+++ maze.js
@@ -20,17 +20,35 @@
 				}
 				/*Line 24 and 26 works*/
 				document.getElementById("end").onmouseover = function(){
-if (document.getElementsByClassName("youlose").length === 0)
-			{
-			window.alert("You win");
+				if (document.getElementsByClassName("youlose").length === 0){
+					window.alert("You win");
+			} else{
+				window.alert("You lose");
+				}
 			}
+
+
+
+			/*document.getElementById("start").onclick = function(){
+			var m = document.getElementsByClassName("boundary").length - 1;
+			var p;
+			for (p=0;p>m.length;m--){
+					m[p].classList.remove("youlose"); 
+					}
+					
+				}*/
+		/*while (m >= 0)
+		{
+			document.querySelectorAll(".boundary")[i].classList.remove("youlose");
+	 		m--;
+	    }*/
 			/*<h2 id="status">"You win"</h2>
 			<h2 id="status">"You lose"</h2>
 			document.getElementById("status").innerHTML="You win";
 			document.getElementById("status").innerHTML="You lose";
 				} };
 			*/
-		}}
+		}
 
 		
 
