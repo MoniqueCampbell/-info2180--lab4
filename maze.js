111e31ce158eab694abe8d09ede7ebe684d60684
MoniqueCampbell <campbellmonique48@yahoo.com>
2018-10-22 19:56:36 -0500

    Part 6 and Fix Part 2
    The maze now turns red when the user leaves the maze. Part 2 was discovered to not be working properly which is now fixed.

--- maze.js
+++ maze.js
@@ -6,20 +6,22 @@ window.onload = function(){
 	}	
 			
 	//Part 2 and Part 5
-	document.querySelector(".boundary").onmouseover = function(){
-		var x=document.querySelectorAll(".boundary");
-		var i;
-		for (i=0;i<x.length;i++){
-			x[i].classList.add("youlose"); 
-		}	
-		document.body.append(document.createElement("h2").textContent = "You Lose!");			
+	var x = document.querySelectorAll("div.boundary");
+	for (var i = 0;i < x.length;i++){
+		x[i].onmouseover = function(){
+			var x = document.querySelectorAll("div.boundary");
+			for (var i = 0;i < x.length;i++){
+				x[i].classList.add("youlose"); 
+			}	
+			document.body.append(document.createElement("h2").textContent = "You Lose!");			
+		}
 	}
 				
 	//Part 3 and Part 5
 	document.getElementById("end").onmouseover = function(){
 		if (document.getElementsByClassName("youlose").length === 0){
 			/*window.alert("You win");*/
-			 document.body.append(document.createElement("h2").textContent = "You Win!");
+			document.body.append(document.createElement("h2").textContent = "You Win!");
 		}
 		else{
 			/*window.alert("You lose");*/
@@ -35,11 +37,12 @@ window.onload = function(){
 	 		m--;
 	    }
 	}
+
+	//Part 6
+	document.getElementById("maze").onmouseleave = function(){
+		for (var n = 0; n < document.getElementsByClassName("boundary").length - 1; n++){
+			document.querySelectorAll("div.boundary")[n].classList.add("youlose");
+		}
+		document.body.append(document.createElement("h2").textContent = "You Lose!");
+	}
 }
-
-		
-
-
-		
-
-
